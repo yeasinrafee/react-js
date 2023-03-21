af149dc40eb1ca0167b41d9e04af0552dc70b925
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-03-21 23:01:56 +0600

    getting countries using api

---
+++ react-rest-countries/src/App.js
@@ -0,0 +1,29 @@
+
+import { useEffect, useState } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <LoadCountries></LoadCountries>
+    </div>
+  );
+}
+
+function LoadCountries(){
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() =>{
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => setCountries(data));
+  }, [])
+
+  return (
+    <div>
+      <h1>Checking Every Countries of the World!!</h1>
+      <h3>Available Countries: {countries.length}</h3>
+    </div>
+  )
+}
+
+export default App;
