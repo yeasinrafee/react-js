6d568ff851346032392bf5555d7431232ce1d7cf
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-03-22 20:42:19 +0600

    Country compoment ui jsx

--- react-rest-countries/src/App.js
+++ react-rest-countries/src/App.js
@@ -1,29 +1,33 @@
-
-import { useEffect, useState } from 'react';
+import './App.css';
+import Countries from './components/Countries/Countries';
 
 function App() {
   return (
     <div className="App">
-      <LoadCountries></LoadCountries>
+      <Countries></Countries>
     </div>
   );
 }
 
-function LoadCountries(){
-  const [countries, setCountries] = useState([]);
 
-  useEffect(() =>{
-    fetch('https://restcountries.com/v3.1/all')
-    .then(res => res.json())
-    .then(data => setCountries(data));
-  }, [])
 
-  return (
-    <div>
-      <h1>Checking Every Countries of the World!!</h1>
-      <h3>Available Countries: {countries.length}</h3>
-    </div>
-  )
-}
+
+
+// function LoadCountries(){
+//   const [countries, setCountries] = useState([]);
+
+//   useEffect(() =>{
+//     fetch('https://restcountries.com/v3.1/all')
+//     .then(res => res.json())
+//     .then(data => setCountries(data));
+//   }, [])
+
+//   return (
+//     <div>
+//       <h1>Checking Every Countries of the World!!</h1>
+//       <h3>Available Countries: {countries.length}</h3>
+//     </div>
+//   )
+// }
 
 export default App;
--- react-rest-countries/src/components/Country/Country.js
+++ react-rest-countries/src/components/Country/Country.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import './Country.css';
 
-const Country = () => {
+const Country = (props) => {
+    const {name, area, capital, flags, region, population} = props.country;
+    // console.log(props.country);
     return (
         <div>
-            
+            <img src={flags.png} alt="" />
+            <h2>Name: {name.common}</h2>
+            <h3>Region: {region}</h3>
+            <div>
+                <h4>Other Details</h4>
+                <ul>
+                    <li>Area: {area}</li>
+                    <li>Capital: {capital? capital[0]: ''}</li>
+                    <li>Population {population}</li>
+                </ul>
+            </div>
         </div>
     );
 };
